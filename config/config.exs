682428a9a56787b4a60f75deb4bfc2a9f7c691e8
Joshua Hansen <joshua@epicbanality.com>
2021-02-18 11:41:39 -0800

    Move error defaults out of config

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,3 @@
 import Config
 
-import_config "defaults.exs"
 import_config "#{Mix.env()}.exs"
--- lib/resourceful/error.ex
+++ lib/resourceful/error.ex
@@ -85,7 +85,64 @@ defmodule Resourceful.Error do
   implying it was never resolved to an actual attribute.
   """
 
-  @error_type_defaults Application.get_env(:resourceful, :error_type_defaults)
+  @error_type_defaults Enum.into(
+                         Application.get_env(:resourceful, :error_type_defaults) || %{},
+                         %{
+                           attribute_not_found: %{
+                             detail:
+                               "An attribute with the name `%{key}` could not be found for resource type `%{resource_type}`.",
+                             title: "Attribute Could Not Be Found"
+                           },
+                           cannot_filter_by_attribute: %{
+                             detail:
+                               "`%{attribute}` is a valid attribute but cannot be used to filter resource.",
+                             title: "Resource Cannot Be Filtered By Attribute"
+                           },
+                           cannot_sort_by_attribute: %{
+                             detail:
+                               "`%{attribute}` is a valid attribute but cannot be used to sort resource.",
+                             title: "Resource Cannot Be Sorted By Attribute"
+                           },
+                           invalid_filter: %{
+                             title: "Invalid Filter"
+                           },
+                           invalid_filter_operator: %{
+                             detail: "`%{operator}` is not a valid filter operator.",
+                             title: "Invalid Filter Operator"
+                           },
+                           invalid_filter_value: %{
+                             detail:
+                               "`%{value}` is not compatible with filter operator `%{operator}`.",
+                             title: "Invalid Filter Value"
+                           },
+                           invalid_jsonapi_field: %{
+                             detail:
+                               "`%{key}` is not a valid field name for resource type `%{resource_type}`.",
+                             title: "Invalid Field"
+                           },
+                           invalid_jsonapi_field_type: %{
+                             detail: "`%{key}` is not a valid resource type in this request.",
+                             title: "Invalid Field Type"
+                           },
+                           invalid_sorter: %{
+                             title: "Invalid Sorter"
+                           },
+                           max_filters_exceeded: %{
+                             detail:
+                               "Resource cannot be filtered by more than %{max_allowed} filters.",
+                             title: "Max Filters Exceeded"
+                           },
+                           max_sorters_exceeded: %{
+                             detail:
+                               "Resource cannot be sortered by more than %{max_allowed} sorters.",
+                             title: "Max Sorters Exceeded"
+                           },
+                           type_cast_failure: %{
+                             detail: "`%{input}` cannot be cast to type `%{type}`.",
+                             title: "Type Cast Failure"
+                           }
+                         }
+                       )
 
   @doc """
   Mostly a convenience function to use instead of `list/1` with the option to
