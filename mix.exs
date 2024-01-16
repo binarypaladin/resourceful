9948bf5f6c984d68fd236712f4831875111109e0
Joshua Hansen <joshua@epicbanality.com>
2024-01-16 10:39:57 -0700

    Update deps and config, and bump version

--- mix.exs
+++ mix.exs
@@ -6,22 +6,22 @@ defmodule Resourceful.MixProject do
       app: :resourceful,
       deps: deps(),
       description: "A type and query toolkit for web-based APIs.",
-      elixir: "~> 1.10",
+      elixir: "~> 1.14",
       name: "Resourceful",
       package: package(),
       source_url: "https://github.com/binarypaladin/resourceful",
       start_permanent: Mix.env() == :prod,
-      version: "0.1.3"
+      version: "0.1.4"
     ]
   end
 
-  def application, do: [applications: [], extra_applications: [:ecto]]
+  def application, do: []
 
   defp deps do
     [
-      {:ecto, "~> 3.4"},
-      {:ecto_sql, "~> 3.4", only: [:test]},
-      {:inflex, "~> 2.0", only: [:test]},
+      {:ecto, "~> 3.9"},
+      {:ecto_sql, "~> 3.9", only: [:test]},
+      {:inflex, "~> 2.1", only: [:test]},
       {:postgrex, ">= 0.0.0", only: [:test]},
       {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
     ]
